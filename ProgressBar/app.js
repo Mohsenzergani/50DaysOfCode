0aad894d3955f990a5b1a52f34bef7bb948a8586
mohsen Zergni <mohsen.zergani@gmail.com>
2021-03-18 21:38:43 +0330

    Add SetTime Out to progressbar

---
+++ ProgressBar/app.js
@@ -0,0 +1,7 @@
+// get progressBar element
+const progress = document.querySelector(".progress-done");
+
+setTimeout(() =>{
+    progress.style.width = progress.getAttribute("data-done") + '%'
+    progress.style.opacity = 1;
+},500)
