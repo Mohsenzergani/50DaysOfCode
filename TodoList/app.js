ac390f9ae92665298b0586e8270421b3aa4ecc21
Mohsenzergani <mohsen.zergani@gmail.com>
2021-03-29 06:44:07 +0430

    add html css to todolist

---
+++ TodoList/app.js
@@ -0,0 +1,48 @@
+// structure solve problem
+// 1 element haro migirim
+// 2 
+// get elements
+
+const clearBtn = document.querySelector(".clear");
+const toDoList = document.querySelector("#list");
+const toDoInput = document.querySelector("#input");
+const toDoAddBtn = document.querySelector(".fa-plus-circle");
+// -----------------------------------------------------------------
+
+// Selecting the icon class names
+
+
+// -------------------------------------------------------------------------------------------------------------------
+// To Do Container
+// let toDoContainer = [];
+// let id = 0;
+// -------------------------------------------------------------------------------------------------------------------
+
+
+// Clear the local storage
+
+// -------------------------------------------------------------------------------------------------------------------
+// addToDo Function
+
+  // https://developer.mozilla.org/en-US/docs/Web/API/Element/insertAdjacentHTML
+
+
+// addToDo("walk the dogs", 0, false, false);
+// addToDo("walk the dogs", 0, true, false);
+// addToDo("walk the dogs", 0, true, true);
+// -------------------------------------------------------------------------------------------------------------------
+
+// Adding a to-do the list when the enter key is pressed
+
+// Adding a to-do the list when the plus icon is clicked
+
+
+// displayToDo Function
+
+
+      // Persisting to local storage -> updating the local storage
+      // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/JSON/stringify
+  
+// -------------------------------------------------------------------------------------------------------------------
+
+// When a to do is completed
