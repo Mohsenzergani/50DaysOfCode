827ee60caf9058733dba4cdb1bb8c54a1329e2bf
Mohsenzergani <mohsen.zergani@gmail.com>
2021-03-31 08:21:49 +0430

    add Html& css to app

---
+++ TheSimpleDarg&Drop/app.js
@@ -0,0 +1,11 @@
+// 1 first get the elements
+// 2 get lists 
+
+// get elements
+const lists = document.querySelectorAll(".list");
+const listItems = document.querySelectorAll(".list-item");
+
+let draggedItem = null;
+
+// 2 get lists
+for()
