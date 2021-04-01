851e787ff6fb5228912e23a5b420299131b58dd4
Mohsenzergani <mohsen.zergani@gmail.com>
2021-04-01 09:57:53 +0430

    add html & css

---
+++ MemoryCardGame2/app.js
@@ -0,0 +1,69 @@
+  // 1 get all pic and put him in array
+  //card options
+  const cardArray = [
+    {
+      name: 'fries',
+      img: 'images/fries.png'
+    },
+    {
+      name: 'cheeseburger',
+      img: 'images/cheeseburger.png'
+    },
+    {
+      name: 'ice-cream',
+      img: 'images/ice-cream.png'
+    },
+    {
+      name: 'pizza',
+      img: 'images/pizza.png'
+    },
+    {
+      name: 'milkshake',
+      img: 'images/milkshake.png'
+    },
+    {
+      name: 'hotdog',
+      img: 'images/hotdog.png'
+    },
+    {
+      name: 'fries',
+      img: 'images/fries.png'
+    },
+    {
+      name: 'cheeseburger',
+      img: 'images/cheeseburger.png'
+    },
+    {
+      name: 'ice-cream',
+      img: 'images/ice-cream.png'
+    },
+    {
+      name: 'pizza',
+      img: 'images/pizza.png'
+    },
+    {
+      name: 'milkshake',
+      img: 'images/milkshake.png'
+    },
+    {
+      name: 'hotdog',
+      img: 'images/hotdog.png'
+    }
+  ];
+  
+
+// 2 get elements
+const grid = document.querySelector('.grid');
+
+// 3 create your board
+function createBoard(){
+  for(let i =0;i <cardArray.length;i++){
+    let card = document.createElement('img');
+    card.setAttribute('src','images/blank.png');
+    card.setAttribute('data-id',i)
+    console.log(card);
+
+  }
+}
+
+createBoard()
