4ad90438d707fa1bfb77da264946e65bbce23221
Mohsenzergani <mohsen.zergani@gmail.com>
2021-04-02 23:27:32 +0430

    Video Player

---
+++ VidePlayer/script.js
@@ -0,0 +1,60 @@
+// get elements
+const video = document.querySelector("#video");
+const play = document.querySelector("#play");
+const stop = document.querySelector("#stop");
+const progress = document.querySelector("#progress");
+const timestamp = document.querySelector("#timestamp");
+
+// Play & pause video
+function toggleVideStatus() {
+  if (video.paused) {
+    video.play();
+  } else {
+    video.pause();
+  }
+}
+// update play/pause icon
+
+function updatePlayIcon() {
+  if (video.paused) {
+    play.innerHTML = '<i class="fa fa-play fa-2x"></i>';
+  } else {
+    play.innerHTML = '<i class="fa fa-pause fa-2x"></i>';
+  }
+}
+// Update progress & timestamp
+function updateProgress() {
+  progress.value = (video.currentTime / video.duration) * 100;
+
+  // Get minutes
+  let mins = Math.floor(video.currentTime / 60);
+  if (mins < 10) {
+    mins = '0' + String(mins);
+  }
+
+  // Get seconds
+  let secs = Math.floor(video.currentTime % 60);
+  if (secs < 10) {
+    secs = '0' + String(secs);
+  }
+
+  timestamp.innerHTML = `${mins}:${secs}`;
+}
+// Set video time to progress
+function setVideoProgress() {
+  video.currentTime = (+progress.value * video.duration) / 100;
+}
+// stop Video
+function stopVideo() {
+  video.currentTime = 0;
+  video.pause();
+}
+// 1---- addEventListener for video
+video.addEventListener("click", toggleVideStatus);
+video.addEventListener("pause", updatePlayIcon);
+video.addEventListener("play", updatePlayIcon);
+video.addEventListener("timestamp", updateProgress);
+
+play.addEventListener("click", toggleVideStatus);
+stop.addEventListener("click", stopVideo);
+progress.addEventListener("change", setVideoProgress);
