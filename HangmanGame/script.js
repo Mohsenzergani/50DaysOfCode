3731e1eff4ad6efc122e13546229eb890305af80
Mohsenzergani <mohsen.zergani@gmail.com>
2021-04-08 12:17:19 +0430

    Hangman

---
+++ HangmanGame/script.js
@@ -0,0 +1,12 @@
+// get elements
+const worldEl = document.querySelector("#word");
+const wrongLettersEl = document.querySelector("#wrong-letters");
+const playAgainBtn = document.querySelector('#play-button');
+const popup = document.querySelector("#popup-container");
+const notification = document.querySelector("#notification-container");
+const finalMessage = document.querySelector("#final-message");
+
+const figureParts = document.querySelectorAll(".figure-parts");
+
+
+const words = ['application','programming','interface','wizard']; 
