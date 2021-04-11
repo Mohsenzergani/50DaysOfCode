42f3530523828bf98304f106e247182d6d2d1261
Mohsenzergani <mohsen.zergani@gmail.com>
2021-04-11 16:04:54 +0430

    Hangman Game

--- HangmanGame/script.js
+++ HangmanGame/script.js
@@ -1,13 +1,13 @@
-const wordEl = document.getElementById('word');
-const wrongLettersEl = document.getElementById('wrong-letters');
-const playAgainBtn = document.getElementById('play-button');
-const popup = document.getElementById('popup-container');
-const notification = document.getElementById('notification-container');
-const finalMessage = document.getElementById('final-message');
+const wordEl = document.getElementById("word");
+const wrongLettersEl = document.getElementById("wrong-letters");
+const playAgainBtn = document.getElementById("play-button");
+const popup = document.getElementById("popup-container");
+const notification = document.getElementById("notification-container");
+const finalMessage = document.getElementById("final-message");
 
-const figureParts = document.querySelectorAll('.figure-part');
+const figureParts = document.querySelectorAll(".figure-part");
 
-const words = ['application', 'programming', 'interface', 'wizard'];
+const words = ["application", "programming", "interface", "wizard"];
 
 let selectedWord = words[Math.floor(Math.random() * words.length)];
 
@@ -18,70 +18,67 @@ const wrongLetters = [];
 function displayWord() {
   wordEl.innerHTML = `
     ${selectedWord
-      .split('')
+      .split("")
       .map(
-        letter => `
+        (letter) => `
           <span class="letter">
-            ${correctLetters.includes(letter) ? letter : ''}
+            ${correctLetters.includes(letter) ? letter : ""}
           </span>
         `
       )
-      .join('')}
+      .join("")}
   `;
 
-  const innerWord = wordEl.innerText.replace(/\n/g, '');
+  const innerWord = wordEl.innerText.replace(/\n/g, "");
 
   if (innerWord === selectedWord) {
-    finalMessage.innerText = 'Congratulations! You won! 😃';
-    popup.style.display = 'flex';
+    finalMessage.innerText = "Congratulations! You won! 😃";
+    popup.style.display = "flex";
   }
 }
 
-// Update the wrong letters
+// update the wrong letter
 function updateWrongLettersEl() {
-  // Display wrong letters
+  
+  // first--- job show the wrong word
   wrongLettersEl.innerHTML = `
-    ${wrongLetters.length > 0 ? '<p>Wrong</p>' : ''}
-    ${wrongLetters.map(letter => `<span>${letter}</span>`)}
+  ${wrongLetters.length > 0 ? "<p>Wrong</p>" : ''}
+  ${wrongLetters.map(lett => `<span>${lett}</span>`)}
   `;
-
-  // Display parts
-  figureParts.forEach((part, index) => {
+  // two--- show the figure
+  figureParts.forEach((part,index) => {
     const errors = wrongLetters.length;
-
-    if (index < errors) {
-      part.style.display = 'block';
-    } else {
-      part.style.display = 'none';
+    if(index < errors){
+      part.style.display = "block"
+    }else {
+      part.style.display = "none";
     }
-  });
-
-  // Check if lost
-  if (wrongLetters.length === figureParts.length) {
-    finalMessage.innerText = 'Unfortunately you lost. 😕';
-    popup.style.display = 'flex';
+  })
+  //3 --- check if lost
+  if(wrongLetters.length === figureParts.length){
+    finalMessage.innerHTML = 'Unfortunately you lost. 😕';
+    popup.style.display = 'flex'
   }
+
 }
 
-// Show notification
+// show notification
 function showNotification() {
-  notification.classList.add('show');
+  notification.classList.add("show");
 
   setTimeout(() => {
-    notification.classList.remove('show');
+    notification.classList.remove("show");
   }, 2000);
 }
 
 // Keydown letter press
-window.addEventListener('keydown', e => {
+window.addEventListener("keydown", (e) => {
   // console.log(e.keyCode);
   if (e.keyCode >= 65 && e.keyCode <= 90) {
     const letter = e.key;
-
     if (selectedWord.includes(letter)) {
       if (!correctLetters.includes(letter)) {
         correctLetters.push(letter);
-
         displayWord();
       } else {
         showNotification();
@@ -89,7 +86,6 @@ window.addEventListener('keydown', e => {
     } else {
       if (!wrongLetters.includes(letter)) {
         wrongLetters.push(letter);
-
         updateWrongLettersEl();
       } else {
         showNotification();
@@ -97,20 +93,15 @@ window.addEventListener('keydown', e => {
     }
   }
 });
-
-// Restart game and play again
-playAgainBtn.addEventListener('click', () => {
-  //  Empty arrays
-  correctLetters.splice(0);
+// Restart game and play again 
+playAgainBtn.addEventListener('click',() => {
+  // page relod bashe and az aval bazi koim
+  // empty the two arrays
+  correctLetters.splice(0)
   wrongLetters.splice(0);
-
-  selectedWord = words[Math.floor(Math.random() * words.length)];
-
+  selectedWord = words[Math.floor(Math.random() * words.length )]
   displayWord();
-
   updateWrongLettersEl();
-
   popup.style.display = 'none';
-});
-
+})
 displayWord();
