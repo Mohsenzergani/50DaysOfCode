61786a2fb5adf3bc3c2fdf9f3713447708d0c419
Mohsenzergani <mohsen.zergani@gmail.com>
2021-04-15 13:34:37 +0430

    finish api dog

--- RandomAnimalImage/app.js
+++ RandomAnimalImage/app.js
@@ -11,12 +11,19 @@ const foxBtn = document.querySelector('.get-fox');
 // https://aws.random.cat/meow
 
 catBtn.addEventListener("click",getCat);
-
+dogBtn.addEventListener("click",getDog);
 
 // get cat 
 async function getCat(){
   const res = await fetch(`https://aws.random.cat/meow`);
   const data = await res.json();
-  console.log(data);
+ cat.innerHTML = `<img src="${data.file}"/>`
+}
+// get dog pic api
+async function getDog(){
+  const res = await fetch(`https://dog.ceo/api/breeds/image/random`)
+  const data = await res.json();
+ dog.innerHTML = `<img src="${data.message}"/>`;
+//  console.log(data.message)
 }
 
