a34ec162af06667b0aaedbf0c3c4b500726def6a
Mohsenzergani <mohsen.zergani@gmail.com>
2021-04-21 18:29:06 +0430

    ThePongGame2AI

--- ThePongGameVsAl/app.js
+++ ThePongGameVsAl/app.js
@@ -14,36 +14,38 @@ wall.src = "sounds/wall.mp3";
 AIScore.src = "sounds/AIScore.mp3";
 RIScore.src = "sounds/RIScore.mp3";
 
-// the RI player Paddle
+// The RI Player Paddle
 const playerPaddleRI = {
   xP: 0,
-  yP: canvasEl.height / 2 - 100 / 2, // for paddle player
+  yP: canvasEl.height / 2 - 100 / 2,
   height: 100,
   width: 10,
   color: "#d2e603",
   score: 0,
 };
-// the Al player Paddle
+
+// The AI Player Paddle
 const playerPaddleAI = {
   xP: canvasEl.width - 10,
-  yP: canvasEl.height / 2 - 100 / 2, // for paddle player
+  yP: canvasEl.height / 2 - 100 / 2,
   height: 100,
   width: 10,
   color: "orange",
   score: 0,
 };
 
-// Creating the ball
+// Creating the Ball
 const ball = {
   xP: canvasEl.width / 2,
   yP: canvasEl.height / 2,
   radius: 10,
-  speed: 7,
-  xV: 5, //x Velocity
-  yV: 5, // y velocity
+  speed: 35,
+  xV: 5,
+  yV: 5,
   color: "white",
 };
-// Creating net
+
+// Creating the new
 const net = {
   xP: canvasEl.width / 2 - 1,
   yP: 0,
@@ -51,44 +53,43 @@ const net = {
   height: 10,
   color: "white",
 };
-// Drawing the canvas / first element the game
+
+// Drawing the canvas
 function drawRect(xP, yP, width, height, color) {
   canvasContext.fillStyle = color;
   canvasContext.fillRect(xP, yP, width, height);
 }
-// Drawing The circle / second element of game
 
+// Drawing a circle
 function drawCircle(xP, yP, radius, color) {
-  // xp -->  top down x position // yP  left right Y position
   canvasContext.fillStyle = color;
   canvasContext.beginPath();
   canvasContext.arc(xP, yP, radius, 0, Math.PI * 2);
   canvasContext.fill();
 }
 
-// Drawing the text with
+// Drawing the text
 function drawText(content, xP, yP, color) {
   canvasContext.fillStyle = color;
   canvasContext.font = "35px sans-serif";
   canvasContext.fillText(content, xP, yP);
 }
+
 // Drawing the net
 function drawNet() {
-  // drawing the net of center of game
   for (let i = 0; i < canvasEl.height; i += 15) {
-    // ever net 10px height + 5px for gap === 15px
     drawRect(net.xP, net.yP + i, net.width, net.height, net.color);
   }
 }
 
-// runGame function Aka the game loop
-
+// runGame Function AKA The Game Loop
 function runGame() {
   // clearing the canvas
-  drawRect(0, 0, canvasEl.width, canvasEl.height, "#4383a0");
+  drawRect(0, 0, canvasEl.width, canvasEl.height, "#4683a0");
 
-  // Draw net
+  // Draw net function
   drawNet();
+
   // draw score function
   drawText(
     playerPaddleRI.score,
@@ -102,7 +103,8 @@ function runGame() {
     (1 * canvasEl.height) / 10,
     "white"
   );
-  // Drawing the paddle for RI and AI
+
+  // drawing the paddles for RI and AI
   drawRect(
     playerPaddleRI.xP,
     playerPaddleRI.yP,
@@ -110,6 +112,7 @@ function runGame() {
     playerPaddleRI.height,
     playerPaddleRI.color
   );
+
   drawRect(
     playerPaddleAI.xP,
     playerPaddleAI.yP,
@@ -117,28 +120,25 @@ function runGame() {
     playerPaddleAI.height,
     playerPaddleAI.color
   );
-  // Drawing the ball
+
+  // drawing the ball
   drawCircle(ball.xP, ball.yP, ball.radius, ball.color);
 }
-// create the AI
-let intelligenceLevel = 0.1;
-playerPaddleAI.yP +=
-  (ball.yP - (playerPaddleAI.yP + playerPaddleAI.height / 2)) *
-  intelligenceLevel;
-// the player paddle RI event listeners
-canvasEl.addEventListener("mousemove", mousePaddle);
-function mousePaddle(e) {
-  let canvasRect = canvasEl.getBoundingClientRect();
-  playerPaddleRI.yP = e.clientY - canvasRect.top - playerPaddleRI.height / 2;
-}
-// the detection on ball to paddle
-function paddleDetection(BALL, PADDLE) {
-  // ball
+
+// The player Paddle RI Event Listener
+// canvasEl.addEventListener("mousemove", movePaddle);
+// function movePaddle(e) {
+//   let canvasRect = canvasEl.getBoundingClientRect();
+//   playerPaddleRI.yP = e.clientY - canvasRect.top - playerPaddleRI.height / 2;
+// }
+
+// The Collision Detection of Paddles Function
+function paddleColliDete(BALL, PADDLE) {
   BALL.top = BALL.yP - BALL.radius;
   BALL.bottom = BALL.yP + BALL.radius;
   BALL.left = BALL.xP - BALL.radius;
   BALL.right = BALL.xP + BALL.radius;
-  // paddle
+
   PADDLE.top = PADDLE.yP;
   PADDLE.bottom = PADDLE.yP + PADDLE.height;
   PADDLE.left = PADDLE.xP;
@@ -151,23 +151,86 @@ function paddleDetection(BALL, PADDLE) {
     BALL.top < PADDLE.bottom
   );
 }
-// the everything Manger Function
-function everythingManger() {
+
+// the resetBall function
+function resetBall() {
+  ball.xP = canvasEl.width / 2;
+  ball.yP = canvasEl.height / 2;
+
+  ball.speed = 20;
+}
+
+// The everything Manager Function
+function everythingManager() {
   // moving the ball by the amount of acceleration
   ball.xP += ball.xV;
   ball.yP += ball.yV;
+
+  // creating the AI
+  let intelligenceLevel = 0.6;
+  playerPaddleAI.yP +=
+    (ball.yP - (playerPaddleAI.yP + playerPaddleAI.height / 2)) *
+    intelligenceLevel;
+
+    // creating the AI
+  let intelligenceLevel2 = 0.7;
+  playerPaddleRI.yP +=
+    (ball.yP - (playerPaddleRI.yP + playerPaddleRI.height / 2)) *
+    intelligenceLevel2;
   // bouncing off the top and bottom walls
   if (ball.yP + ball.radius > canvasEl.height || ball.yP - ball.radius < 0) {
     ball.yV = -ball.yV;
-    // wall.play();
+    wall.play();
   }
-  let player = ball.xP < canvasEl.width / 2 ? playerPaddleRI : playerPaddleAI;
+
+  let player =
+    ball.xP + ball.radius < canvasEl.width / 2
+      ? playerPaddleRI
+      : playerPaddleAI;
+
+  if (paddleColliDete(ball, player)) {
+    hit.play();
+
+    // when the ball hits the paddle of any player
+    let collisionPoint = ball.yP - (player.yP + player.height / 2);
+
+    // normalization -> converting -50 & 50 -> -1 & 1 & 0
+    collisionPoint = collisionPoint / (player.height / 2);
+
+    // calculating the angle at which the bounces back (radians)
+    let bounceAngle = (collisionPoint * Math.PI) / 1;
+
+    // calculating the direction of the ball when it bounces back
+    let direction = ball.xP + ball.radius < canvasEl.width / 2 ? 1 : -1;
+
+    // updating the velocity when the ball hits any paddle
+    //https://www.mathsisfun.com/sine-cosine-tangent.html
+    ball.xV = direction * ball.speed * Math.cos(bounceAngle);
+    ball.yV = ball.speed * Math.sin(bounceAngle);
+
+    // after each bounce back, the speed of the ball should be increased
+    ball.speed += 0.1;
+  }
+
+  // updating the scores
+  if (ball.xP + ball.radius < 0) {
+    // the AI scored
+    playerPaddleAI.score++;
+    // AIScore.play();
+    resetBall();
+  } else if (ball.xP - ball.radius > canvasEl.width) {
+    // the RI scored
+    playerPaddleRI.score++;
+    RIScore.play();
+    resetBall();
+  }
 }
-// the game initialization function
+
+// The Game Initialization function
 function gameInit() {
-  everythingManger();
+  everythingManager();
   runGame();
 }
-// looping the game to keep running
+// Looping the game to keep it running
 const FPS = 60;
 setInterval(gameInit, 1000 / FPS);
