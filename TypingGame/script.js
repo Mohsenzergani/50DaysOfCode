90a19df1f4f5c92e6558f87157a5162847633991
Mohsenzergani <mohsen.zergani@gmail.com>
2021-04-24 20:52:08 +0430

    Add game over to game

--- TypingGame/script.js
+++ TypingGame/script.js
@@ -8,7 +8,29 @@ const settings = document.getElementById('settings');
 const settingsForm = document.getElementById('settings-form');
 const difficultySelect = document.getElementById('difficulty');
 
-
+// List of words for game
+const words = [
+  'sigh',
+  'tense',
+  'airplane',
+  'ball',
+  'pies',
+  'juice',
+  'warlike',
+  'bad',
+  'north',
+  'dependent',
+  'steer',
+  'silver',
+  'highfalutin',
+  'superficial',
+  'quince',
+  'eight',
+  'feeble',
+  'admit',
+  'drag',
+  'loving'
+];
 
 // Init word
 let randomWord;
@@ -54,17 +76,7 @@ function updateScore() {
   scoreEl.innerHTML = score;
 }
 
-// Update time
-function updateTime() {
-  time--;
-  timeEl.innerHTML = time + 's';
 
-  if (time === 0) {
-    clearInterval(timeInterval);
-    // end game
-    gameOver();
-  }
-}
 
 // Game over, show end screen
 function gameOver() {
