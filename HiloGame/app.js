5004b98bd282e4024295719bed62eef6f5069926
Mohsenzergani <mohsen.zergani@gmail.com>
2021-04-26 11:04:02 +0430

    Game Card

--- HiloGame/app.js
+++ HiloGame/app.js
@@ -1,66 +1,94 @@
 // get element
-
-const message = document.querySelector('.message');
-const score = document.querySelector('.score');
-const button = document.querySelectorAll('button');
-const gamePlay = document.querySelector(".gamePlay");
-// console.log(gamePlay);
-
+const message = document.querySelector(".message");
+const score = document.querySelector(".score");
+const button = document.querySelectorAll("button");
+const gameplay = document.querySelector(".gameplay");
 // variable
 let curCardValue = 0;
 let scoreValue = 0;
 let deck = [];
-const ranks = [2,3,4,5,6,7,8,9,10,"J","Q","K","A"];
-const suits = ["hearts",'diams','clubs',"spades"];
-
-button.forEach(el => el.addEventListener('click',playGame))
-
-// playGame 
-function playGame(e){
-    // 
-    let temp = e.target.innerHTML;
+const ranks = [2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K", "A"];
+const suits = ["hearts", "diams", "clubs", "spades"];
+for (let i = 0; i < button.length; i++) {
+    button[i].addEventListener("click", playGame);
+}
+// toggle function
+function toggleButtons() {
+    button[0].classList.toggle("hideButton");
+    button[1].classList.toggle("hideButton");
+    button[2].classList.toggle("hideButton");
+}
+// play game
+function playGame(e) {
+    let temp = e.target.innerText;
     let myCard = drawCard();
-    if(temp == 'Start'){
-        console.log("you click start");
-        message.innerHTML = 'Higher Or Lower';
-        gamePlay.innerHTML = '';
-        makeCard();
-        toggleButton()
+    if (temp == "Start") {
+        message.innerHTML = "Higher or Lower";
+        gameplay.innerHTML = "";
+        makeCard(myCard);
+        toggleButtons();
+        return;
     }
+  
+    if(myCard.value == curCardValue){
+        message.innerHTML = "Draw";
+    }else{
+        if((temp=="Higher" && (myCard.value >curCardValue)) || (temp == "Lower" && (myCard.value<curCardValue))){
+            scoreValue++;
+            score.innerHTML = scoreValue;
+            message.innerHTML = "Correct, Next?";
+        }else{
+            message.innerHTML = "Wrong Game Over";
+            toggleButtons();
+        }
+    }
+    makeCard(myCard);
+    
 }
-// toggle button
-function toggleButton(){
-   button[0].classList.toggle("hideButton");
-   button[1].classList.toggle("hideButton");
-   button[2].classList.toggle("hideButton");
-
-}
-
-// function makeCard
-function makeCard(){
-    if(deck.length > 0){
-        let card="";
-        return card
-    }else {
+// draw function
+function drawCard() {
+    if (deck.length > 0) {
+        let randIndex = Math.floor(Math.random() * deck.length);
+        let card = deck.splice(randIndex, 1)[0];
+        return card;
+    }
+    else {
         makeDeck();
         return drawCard();
     }
 }
-
-// make deck
-function makeDeck(){
+// make deck Function
+function makeDeck() {
     deck = [];
-    for(let i= 0;i<suits.length;i++){
-        for(let j=0;j<ranks.length;j++){
-            let card ={};
-            card.suits = suits[i];
-            card.rank = ranks[i];
-            card.value = (j+1);
+    for (let i = 0; i < suits.length; i++) {
+        for (let j = 0; j < ranks.length; j++) {
+            let card = {};
+            card.suit = suits[i];
+            card.rank = ranks[j];
+            card.value = (j + 1);
             deck.push(card);
         }
     }
 }
-// Draw Card
-function drawCard(){
-    
+// make card function
+function makeCard(card) {
+    let html1 = card.rank + "<br>&" + card.suit + ";";
+    let html2 = card.rank + "&" + card.suit + ";";
+    let curCards = document.querySelectorAll(".card");
+    let div = document.createElement("div");
+    div.setAttribute("class", "card");
+    div.style.left = (curCards.length * 25) + "px";
+    curCardValue = card.value;
+    if (card.suit === "hearts" || card.suit === "diams") {
+        div.classList.add("red");
+    }
+    let span1 = document.createElement("span");
+    span1.setAttribute("class", "tiny");
+    span1.innerHTML = html2;
+    div.appendChild(span1);
+    let span2 = document.createElement("span");
+    span2.setAttribute("class", "big");
+    span2.innerHTML = html1;
+    div.appendChild(span2);
+    gameplay.appendChild(div);
 }
