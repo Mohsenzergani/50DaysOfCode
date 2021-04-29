e27700aeefd489b22a54d81abeb0b6d1d7f219b7
Mohsenzergani <mohsen.zergani@gmail.com>
2021-04-29 23:44:56 +0430

    add js

--- BudgetApp/app.js
+++ BudgetApp/app.js
@@ -1,9 +1,15 @@
+const balanceEl = document.querySelector(".balance .value");
+const incomeTotalEl = document.querySelector(".income-total");
+const outcomeTotalEl = document.querySelector(".outcome-total");
+const incomeEl = document.querySelector("#income-tracker");
+const expenseEl = document.querySelector("#expense-tracker");
+const allEl = document.querySelector("#all");
+const incomeList = document.querySelector("#income-tracker .list");
+const expenseList = document.querySelector("#expense-tracker .list");
+const allList = document.querySelector("#all .list");
+const lists = document.querySelectorAll(".list");
 
 
-// Tabs
-const expenseBtn = document.querySelector(".tab1");
-const incomeBtn = document.querySelector(".tab2");
-const allBtn = document.querySelector(".tab3");
 
 // Input Btns
 const addExpense = document.querySelector(".add-expense");
